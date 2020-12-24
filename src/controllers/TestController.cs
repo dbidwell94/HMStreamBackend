00f035fdfe5f7d0a975b14b76150d6cd5c40fad5
Devin Bidwell <devin.bidwell@accretivetg.com>
2020-12-23 23:06:45 -0800

    Added more test endpoints

--- src/controllers/TestController.cs
+++ src/controllers/TestController.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 using SimpleServer.Attributes;
 using SimpleServer.Networking.Data;
 
 namespace HMStreamBackend.Controllers
 {
+    #nullable enable
+    struct EndpointTest
+    {
+        public string? something;
+        public IEnumerable<int>? someArr;
+
+        public EndpointTest(string something, IEnumerable<int> someArr)
+        {
+            this.something = something;
+            this.someArr = someArr;
+        }
+    }
+    #nullable disable
+
     [RestController("/")]
     class TestController
     {
@@ -16,8 +31,23 @@ namespace HMStreamBackend.Controllers
         [GetMapping("/anothertest", Produces = MediaTypes.ApplicationJson, Accepts = MediaTypes.ApplicationJson)]
         public ResponseEntity AnotherTestMessage()
         {
-            return new ResponseEntity("Another test");
-            
+            int amount = 50;
+            EndpointTest[] testArr = new EndpointTest[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                testArr[i] = new EndpointTest
+                {
+                    something = $"This is a test to see if {i} works",
+                    someArr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }
+                };
+            }
+            return new ResponseEntity(testArr);
+        }
+
+        [GetMapping("/param-test", Produces = MediaTypes.ApplicationJson, Accepts = MediaTypes.ApplicationJson)]
+        public ResponseEntity ParameterTest(EndpointTest test)
+        {
+            return new ResponseEntity(test);
         }
     }
 }
